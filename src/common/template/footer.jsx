03eae1376cf3b8f572f1b3e42e63d3089fac6b59
Smooke09 <pedrolucas1014zz@gmail.com>
2022-06-23 12:28:47 -0300

    Navigation with React-router

--- src/common/template/footer.jsx
+++ src/common/template/footer.jsx
@@ -4,7 +4,9 @@ export default (props) => (
   <footer className="main-footer">
     <strong>
       Copyright &copy; 2022
-      <a href="https://github.com/Smooke09"> Pedro Lucas</a>
+      <a href="https://github.com/Smooke09" target="_blank">
+        Pedro Lucas
+      </a>
     </strong>
   </footer>
 );
