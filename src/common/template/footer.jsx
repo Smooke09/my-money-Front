f8a34b8a62022bcb7386f769464292ae8a9f282d
Smooke09 <pedrolucas1014zz@gmail.com>
2022-06-23 12:15:22 -0300

    Component footer

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default (props) => (
+  <footer className="main-footer">
+    <strong>
+      Copyright &copy; 2022
+      <a href="https://github.com/Smooke09"> Pedro Lucas</a>
+    </strong>
+  </footer>
+);
