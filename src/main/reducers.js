2020b2ff803326e3a889aee475380e4e05c87147
Smooke09 <pedrolucas1014zz@gmail.com>
2022-06-24 11:21:05 -0300

    Action primary o redux

--- src/main/reducers.js
+++ src/main/reducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import DashboardReducer from "../dashboard/dashboardReducer";
+import tabReducer from "../common/tab/tabReducer";
 
 const rootReducer = combineReducers({
   dashboard: DashboardReducer,
+  tab: tabReducer,
 });
 
 export default rootReducer;
