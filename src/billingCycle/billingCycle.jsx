6f80e15889d45ab7817b4bacba7e986be54f5a48
Smooke09 <pedrolucas1014zz@gmail.com>
2022-06-28 09:09:01 -0300

    creditList component evolution

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -8,17 +8,15 @@ import Tabs from "../common/tab/tabs";
 import TabsHeader from "../common/tab/tabsHeader";
 import TabsContent from "../common/tab/tabsContent";
 import TabHeader from "../common/tab/tabHeader";
-import { selectTab, showTabs } from "../common/tab/tabActions";
 import TabContent from "../common/tab/tabContent";
-import { create, update, remove } from "./billingCycleActions";
+import { init, create, update, remove } from "./billingCycleActions";
 
 import List from "./billingCycleList";
 import Form from "./billingCycleForm";
 
 class BillingCycle extends Component {
   componentWillMount() {
-    this.props.selectTab("tabList");
-    this.props.showTabs("tabList", "tabCreate");
+    this.props.init();
   }
 
   render() {
@@ -68,5 +66,5 @@ class BillingCycle extends Component {
 }
 
 const mapDispatchToProps = (dispatch) =>
-  bindActionCreators({ selectTab, showTabs, create, update, remove }, dispatch);
+  bindActionCreators({ init, create, update, remove }, dispatch);
 export default connect(null, mapDispatchToProps)(BillingCycle);
