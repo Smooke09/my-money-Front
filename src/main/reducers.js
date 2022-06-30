1f7ef6ac80224c1a5941fcf36facc136cef27de4
Smooke09 <pedrolucas1014zz@gmail.com>
2022-06-30 09:42:48 -0300

    adding authReduce component

--- src/main/reducers.js
+++ src/main/reducers.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 import { reducer as toastrReducer } from "react-redux-toastr";
+import AuthReducer from "../auth/authReducer";
 
 import DashboardReducer from "../dashboard/dashboardReducer";
 import tabReducer from "../common/tab/tabReducer";
@@ -13,6 +14,7 @@ const rootReducer = combineReducers({
   billingCycle: BillingCycleReduce,
   form: formReducer,
   toastr: toastrReducer,
+  auth: AuthReducer,
 });
 
 export default rootReducer;
